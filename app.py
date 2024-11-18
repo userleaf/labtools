2e206f01637c15d5d178aa96db35116d0675b747
userleaf <74879416+userleaf@users.noreply.github.com>
2024-11-18 14:58:30 +0300

    commit

--- app.py
+++ app.py
@@ -54,6 +54,5 @@ def generate_csv():
             os.remove(file_path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
     app.run(host="0.0.0.0", port=port, debug=True)
